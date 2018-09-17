f6f890008504e1af47f00ad9b053a5dbd6ede270
William Goncalves <william.goncalves@cwi.com.br>
2018-09-17 00:42:38 -0300

    Add Detail View and fix depencies files

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList';
+import DetailView from './DetailView';
+import Pokemon from '../Pokemon';
+import Pokedex from './Pokedex';
 import './styles/App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {};
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.pokemonMock = new Pokemon({
+      id: "0",
+      name: "Pokemon dont selected",
+      types: [{
+        type: {
+          name: "Pokemon dont selected"
+        }
+      }],
+      sprites: {
+        front_default: "../assets/default.jpg"
+      }
+    });
+  }
+  handleOnClick(id) {
+    var proxy_url = 'https://cors-anywhere.herokuapp.com/';
+    fetch(`${proxy_url}https://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+        this.setState({ pokemon });
+      })
+      .catch(err => console.log(err));
   }
   render() {
     return (
       <div className="App">
-        <PokeList />
+        <Pokedex />
+        <PokeList handleOnClick={this.handleOnClick} />
+        <DetailView pokemon={this.state.pokemon || this.pokemonMock} />
       </div>
     );
   }
--- src/components/PokeCell.js
+++ src/components/PokeCell.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './styles/PokeCell.css';
 import sprites from '../assets/sprites.png';
 
-const PokeCell = ({ pokeClass }) => {
+const PokeCell = ({ pokeClass, handleOnClick }) => {
     const { id, backgroundPosition } = pokeClass;
     const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
-    return <button style={style} className="poke-cell"></button>
+    return <button onClick={() => handleOnClick(id)} style={style} className="poke-cell"></button>
 };
 
 export default PokeCell;
