c0fa2b131e59c598e19321ca41766c97579895d1
William Goncalves <william.goncalves@cwi.com.br>
2018-09-16 22:45:41 -0300

    Struture initial of project

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PokeList from './PokeList';
+import './styles/App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <PokeList />
+      </div>
+    );
+  }
+}
+
+export default App;
