f9c4c7d7a0e5b38125ca331a777ca1183d029bdc
William Goncalves <william.goncalves@cwi.com.br>
2018-09-17 00:43:27 -0300

    Fix layout

---
+++ src/components/Pokedex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Pokedex.css';
+
+const Pokedex = () => {
+    return (
+        <div className="header-pokedex">
+            <h1>Pokedex DEMO</h1>
+            <p>Select your Pokemon!</p>
+        </div>
+    )
+};
+
+export default Pokedex;
